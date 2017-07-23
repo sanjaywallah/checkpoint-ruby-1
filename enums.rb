b84d3ce9f2b2c0f374abf34b34ef6bde386b4a5f
Andrew Whitley <awhitley1233@gmail.com>
2017-07-23 14:27:43 -0400

    update enums sections

--- enums.rb
+++ enums.rb
@@ -25,5 +25,5 @@ children = ['Wendy', 'John', 'Michael']
 
 # Use `.map` to iterate through the `children` array and add ` Darling` to the end
 # of their names. Assign the returned array to a variable called `darling_children`.
-# Example: `Wendy` should become `Wendy Darling` in the new array. 
+# Example: `Wendy` should become `Wendy Darling` in the new array.
 # Type your solution directly below this line:
